14e15528f499d7e30589af65ea6359c01903c7ef
Melissari1997 <62259800+Melissari1997@users.noreply.github.com>
2020-04-12 19:16:59 +0200

    Primi cod smell corretti

--- src/firstMilestone/RetrieveTicketsID.java
+++ src/firstMilestone/RetrieveTicketsID.java
@@ -1,4 +1,4 @@
-package firstMilestone;
+package firstmilestone;
 import java.io.BufferedReader;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -6,11 +6,15 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
 import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.List;
 import java.util.TreeMap;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import java.net.*;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -32,34 +36,34 @@ public class RetrieveTicketsID {
 		   }
 
 	   public static JSONArray readJsonArrayFromUrl(String url) throws IOException, JSONException {
-	      InputStream is = new URL(url).openStream();
-	      try {
-	         BufferedReader rd = new BufferedReader(new InputStreamReader(is, Charset.forName("UTF-8")));
-	         String jsonText = readAll(rd);
-	         JSONArray json = new JSONArray(jsonText);
-	         return json;
-	       } finally {
-	         is.close();
-	       }
-	   }
+		   InputStream is = new URL(url).openStream();
+		      try 
+		         (BufferedReader rd = new BufferedReader(new InputStreamReader(is, StandardCharsets.UTF_8));
+		         ){
+		         return new JSONArray(readAll(rd));
+		       } finally {
+		         is.close();
+		       }
+		   }
 
    public static JSONObject readJsonFromUrl(String url) throws IOException, JSONException {
       InputStream is = new URL(url).openStream();
-      try {
-         BufferedReader rd = new BufferedReader(new InputStreamReader(is, Charset.forName("UTF-8")));
-         String jsonText = readAll(rd);
-         JSONObject json = new JSONObject(jsonText);
-         return json;
+      try 
+         (BufferedReader rd = new BufferedReader(new InputStreamReader(is, StandardCharsets.UTF_8));
+         ){
+         return new JSONObject(readAll(rd));
        } finally {
          is.close();
        }
    }
    
    
-   public static ArrayList<String> GetTicketID(String projName) throws IOException, JSONException{
+   public static List<String> GetTicketID(String projName) throws IOException, JSONException{
 	 
-	   Integer j = 0, i = 0, total = 1;
-	   ArrayList<String> ticketList =  new ArrayList<String>();
+	   Integer j = 0;
+	   Integer i = 0; 
+	   Integer total = 1;
+	   List<String> ticketList =  new ArrayList<>();
       //Get JSON API for closed bugs w/ AV in the project
       do {
          //Only gets a max of 1000 at a time, so must do this multiple times if bugs >1000
@@ -88,19 +92,17 @@ public class RetrieveTicketsID {
 	   Integer i = 0;
 	   Integer page = 1;
 	   Integer per_page = 100;
+	   Logger logger = Logger.getLogger(RetrieveTicketsID.class.getName());
 	   int threshold = 7;   
 	   CSVWriter csvWriter = new CSVWriter(new FileWriter(projName + "BugChart.csv"),';',
                CSVWriter.NO_QUOTE_CHARACTER,
                CSVWriter.DEFAULT_ESCAPE_CHARACTER,
                CSVWriter.DEFAULT_LINE_END);
 
-	   ArrayList<String> ticketsID = GetTicketID(projName);
+	   List<String> ticketsID = GetTicketID(projName);
 
       do {
-
-    	  System.out.println("Page: " + page.toString());
-    	  String res = readJsonArrayFromUrl("https://api.github.com/repos/" +organization + "/"+ projName +"/commits?page="+ page.toString()+"&per_page=" + per_page.toString()).toString();
-    	
+    	  String res = readJsonArrayFromUrl("https://api.github.com/repos/" +organization + "/"+ projName +"/commits?page="+ page.toString()+"&per_page=" + per_page.toString()).toString();	
     	  JSONArray jsonArray = new JSONArray(res);
    	      int total = jsonArray.length();
 
@@ -130,9 +132,7 @@ public class RetrieveTicketsID {
               if (start != -1 && end != -1 && start<end) {
             	 
     			  ticketMessage = commitMessage.substring(start,end); // prendo tutto finchè non trovo ] o :
-    			    System.out.println(ticketMessage);
-					System.out.println(date);
-					System.out.println("----------------");
+    			  
     			  if(ticketsID.contains(ticketMessage)) {
     				  	Date data = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss").parse(date); 
 			            Calendar cal = Calendar.getInstance();
@@ -140,9 +140,9 @@ public class RetrieveTicketsID {
 			            Integer month= cal.get(Calendar.MONTH) +1;
 			            Integer year = cal.get(Calendar.YEAR);
 						date = String.valueOf(year) + "/" + String.format("%02d",month);
-			            System.out.println(ticketMessage);
-			            System.out.println(date);
-						System.out.println("----------------");
+						logger.log(Level.INFO, ticketMessage);
+	    			    logger.log(Level.INFO, date);
+	    			    logger.log(Level.INFO, "---------------");
 						if(mapDate.containsKey(date)) {		            	
 			            	mapDate.put(date, mapDate.get(date)+1);
 			            }
@@ -155,10 +155,9 @@ public class RetrieveTicketsID {
     	  page++;
       } while (true);
       mapDate.entrySet().forEach(entry->{
-    	    System.out.println("Data: " + entry.getKey() + " ---> Numero: " + entry.getValue());  
+    	    logger.log(Level.INFO, "Data: " + entry.getKey() + " ---> Numero: " + entry.getValue());  
     	    csvWriter.writeNext(new String[]{entry.getKey() , String.valueOf(entry.getValue())});
       });
       csvWriter.close();
-      //TODO aggiungere commenti.
    }
 }
